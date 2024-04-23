24ae3df0c05087e41d710a607382d2c4eeb97e04
root <alfredo.445566@gmail.com>
2024-04-23 01:58:41 +0000

    get and count methods with their tests

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieves an object"""
+        key = cls.__name__ + "." + id
+        try:
+            return self.all()[key]
+        except KeyError:
+            return None
+
+    def count(self, cls=None):
+        """counts the number of instances in storage"""
+        return len(self.all(cls))
