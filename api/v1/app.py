fd121eecc2c86e34ac8958b561b8b4dc789bc6cd
root <alfredo.445566@gmail.com>
2024-05-21 18:03:44 +0000

    api folder

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that starts a Flask app"""
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes session"""
+    storage.close()
+
+if __name__ == "__main__":
+    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    API_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
