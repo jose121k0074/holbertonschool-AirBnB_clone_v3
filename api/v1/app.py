e38584d82cffd837490876241cc6f3df289fe3f1
root <alfredo.445566@gmail.com>
2024-05-27 17:59:56 +0000

    add error handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask app"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Closes session"""
     storage.close()
+
+@app.errorhandler(404)
+def errorhandler(error):
+    """Returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
