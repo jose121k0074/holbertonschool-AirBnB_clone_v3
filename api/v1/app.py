6b024811b5a56c450667b5c2d80502d97a7223f6
root <alfredo.445566@gmail.com>
2024-05-27 23:11:12 +0000

    add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 """Script that starts a Flask app"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
